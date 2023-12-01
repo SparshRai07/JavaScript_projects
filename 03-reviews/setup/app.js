30582c993bbe816f394ce8e2b45a1c0bfd4813e7
SparshRai07 <cakelllkami@gmail.com>
2023-12-01 23:43:56 +0545

    commit message

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -29,21 +29,22 @@ const reviews = [
     text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
 ];
-// select items
-const img = document.getElementById('person-img');
-const author = document.getElementById('author');
-const job = document.getElementById('job');
-const info = document.getElementById('info');
 
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const randomBtn = document.querySelector('.random-btn');
+// select items
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
 
 // set starting item
 let currentItem = 0;
 
 // load initial item
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
@@ -55,10 +56,11 @@ window.addEventListener('DOMContentLoaded', function () {
 function showPerson(person) {
   const item = reviews[person];
   img.src = item.img;
-  author.textContent = item.name;
+  author.textContent = item.name; // Corrected line
   job.textContent = item.job;
   info.textContent = item.text;
 }
+
 // show next person
 nextBtn.addEventListener('click', function () {
   currentItem++;
@@ -67,6 +69,7 @@ nextBtn.addEventListener('click', function () {
   }
   showPerson(currentItem);
 });
+
 // show prev person
 prevBtn.addEventListener('click', function () {
   currentItem--;
@@ -75,10 +78,9 @@ prevBtn.addEventListener('click', function () {
   }
   showPerson(currentItem);
 });
+
 // show random person
 randomBtn.addEventListener('click', function () {
-  console.log('hello');
-
   currentItem = Math.floor(Math.random() * reviews.length);
   showPerson(currentItem);
 });
