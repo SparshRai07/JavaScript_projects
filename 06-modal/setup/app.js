ea8cd05e7d9c4716a166303f6d5572739d0aedce
SparshRai07 <cakelllkami@gmail.com>
2023-12-03 14:57:31 +0545

    modal

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,14 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const modelBtn = document.querySelector(".modal-btn");
+const model = document.querySelector(".modal-overlay");
+const closeBtn = document.querySelector('.close-btn');
+
+modelBtn.addEventListener("click", function() {
+  modal.classList.add("open-modal");
+});
+closeBtn.addEventListener("click", function(){
+ model.classList.remove("open-moda;");
+});
